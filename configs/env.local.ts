0ca27e5d46a80abb245e07e505d07f714c579bb0
Ryota NIINOMI Takemoto <r21nomi@gmail.com>
2022-05-12 15:54:55 +0900

    Refactor

--- configs/env.local.ts
+++ configs/env.local.ts
@@ -1,6 +1,6 @@
 export const ENV_LOCAL = {
   PROJECT_ENV: process.env.PROJECT_ENV || '',
-  APP_NAME: 'TinySketchesViewer local',
+  APP_NAME: 'DailyCodingViewer local',
   BASE_URL: 'http://localhost:3000',
   API_ENDPOINT: 'http://localhost:9000',
 }
--- configs/env.production.ts
+++ configs/env.production.ts
@@ -1,6 +1,6 @@
 export const ENV_PRD = {
   PROJECT_ENV: process.env.PROJECT_ENV || '',
-  APP_NAME: 'TinySketchesViewer',
-  BASE_URL: 'https://tinysketches.page',
+  APP_NAME: 'DailyCodingViewer',
+  BASE_URL: 'https://daily-coding-viewer.vercel.app',
   API_ENDPOINT: 'https://daily-coding-viewer-api.vercel.app',
 }
