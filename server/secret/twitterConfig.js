40b6a0a2c06216704756f7e2ab47a8f0b204e3de
Ryota NIINOMI Takemoto <r21nomi@gmail.com>
2023-06-08 08:54:16 +0900

    Tweak

--- server/secret/twitterConfig.js
+++ server/secret/twitterConfig.js
@@ -1,7 +1,7 @@
 const twitterConfig = {
-  consumerKey: '7iHF64wLPnTmiNo7FrMCbisxL',
-  consumerSecret: 'bdjubpXDU738dDBbMNuZbfXVP66QFN0kAmFpBYCDcCHUce1FYI',
-  accessTokenKey: '1083647971101618176-UZkRblZTfPoLwxoY5AvAWUbwmsaRMZ',
-  accessTokenSecret: 'b0O7NHWSSkl9iT28mP4GtHwM2FVCLDEQ8LRq6jE1ZBSmh',
+  consumerKey: '',
+  consumerSecret: '',
+  accessTokenKey: '',
+  accessTokenSecret: '',
 }
 module.exports = twitterConfig
